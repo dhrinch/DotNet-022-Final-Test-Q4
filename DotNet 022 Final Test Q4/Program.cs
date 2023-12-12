ae8303fdb247cdc0abf9bda166d91fbecca4453c
dhrinch <18438000+dhrinch@users.noreply.github.com>
2023-12-11 20:20:51 -0500

    Add project files.

---
+++ DotNet 022 Final Test Q4/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace DotNet_022_Final_Test_Q4
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class GenericClassWithConstraint<T> where T : struct
+    {
+        private List<T> GenericCollection;
+
+        public GenericClassWithConstraint()
+        {
+            GenericCollection = new List<T>();
+        }
+
+        public void AddItemToList(T item)
+        {
+            GenericCollection.Add(item);
+        }
+
+        public T GetItemByIdx(int index)
+        {
+            if (index < 0 || index >= GenericCollection.Count)
+            {
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+            return GenericCollection[index];
+        }
+
+        public List<T> GetSortedDescending()
+        {
+            List<T> sortedList = new List<T>(GenericCollection);
+            sortedList.Sort();
+            sortedList.Reverse();
+            return sortedList;
+        }
+    }
+}
